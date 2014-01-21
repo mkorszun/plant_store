0ba8365a7f45b3f120471d0d350b6997131ba4be
Mateusz Korszun <mkorszun@gmail.com>
2014-01-21 13:37:32 +0100

    Check if user exists while getting token

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -2,14 +2,23 @@ class Api::V1::TokensController < ActionController::Base
 
   include Api::TokensHelper
   protect_from_forgery
+  before_action :read_user
 
   def create
-    user = User.find_by_email(params[:email])
-    if user.valid_password?(params[:password])
-      token_str = get_token_for_user(user).token
+    if @user.valid_password?(params[:password])
+      token_str = get_token_for_user(@user).token
       render :json => {:token => token_str}
     else
       render :json => {:errors => 'unauthorized'}, :status => :unauthorized
+    end
+  end
+
+  private
+
+  def read_user
+    @user = User.find_by_email(params[:email])
+    if !@user
+      render :json => {:errors => 'unknown_user'}, :status => :unauthorized
     end
   end
 
